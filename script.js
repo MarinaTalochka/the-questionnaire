e809cd523f8ac561fb82fff69d0b3790ec1c5098
MarinaTalochka <153221203+MarinaTalochka@users.noreply.github.com>
2024-03-13 22:22:34 +0300

    Переделанное д-з10

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
-const form = document.querySelector(".form");
-form.addEventListener("submit", (event) => {
-  // Предотвращает действие браузера по умолчанию. В данном случае — отправку формы
-  // https://learn.javascript.ru/default-browser-action
+form.addEventListener("submit", async (event) => {
+
   event.preventDefault();
 
-  // Здесь твой код
+  try {
+
+    const response = await fetch("https://polinashneider.space/user", {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": "Bearer: MarinaTalochka"
+      },
+      body: JSON.stringify({
+        "name": "Марина",
+        "secondName": "Толочко",
+        "phone": 80336555250,
+        "email": "marina.tolochko.1990@mail.ru",
+        "agree": true
+      })
+    });
+
+    if (response.ok) {
+      alert("Данные успешно отправлены!");
+      form.reset();
+    } else {
+      alert("Произошла ошибка при отправке данных.");
+    }
+  } catch (error) {
+
+    alert("Произошла ошибка при отправке данных.");
+  }
 });
